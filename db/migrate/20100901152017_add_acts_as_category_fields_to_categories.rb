adc751d4863c18c57b1456006b3391b5c597ff92
eduarbo framallo <eduarbo+framallo@tangosource.com>
2010-09-01 11:06:06 -0500

    added new fields to category table necessary for the plugin

---
+++ db/migrate/20100901152017_add_acts_as_category_fields_to_categories.rb
@@ -0,0 +1,15 @@
+class AddActsAsCategoryFieldsToCategories < ActiveRecord::Migration
+  def self.up
+    add_column :categories, :children_count, :integer
+    add_column :categories, :ancestors_count, :integer
+    add_column :categories, :descendants_count, :integer
+    add_column :categories, :hidden, :boolean
+  end
+
+  def self.down
+    remove_column :categories, :children_count
+    remove_column :categories, :ancestors_count
+    remove_column :categories, :descendants_count
+    remove_column :categories, :hidden
+  end
+end
