60b9d9f5ebac2587a4d6bff939b1128cb16c2f7d
eduarbo framallo <eduarbo+framallo@tangosource.com>
2010-09-01 14:18:17 -0500

    improved category tree view on admin

--- app/helpers/admin/categories_helper.rb
+++ app/helpers/admin/categories_helper.rb
@@ -9,4 +9,23 @@ module Admin::CategoriesHelper
     HTML
   end
 
+  def category_tree(all_categories, options = {})
+    config = {:matrix => false, :url => {}, :update => 'tree_rights_remote', :deactivate_links => false}
+    config.update(options) if options.is_a?(Hash)
+    roots = all_categories.map {|c| c unless c.parent}.compact
+    roots.collect { |root| category_tree_category(root, all_categories, config) }
+  end
+  
+  def  category_tree_category(category, all_categories, config)
+    children = all_categories.map {|c| c if c.parent == category}.compact
+    result = '&nbsp;' * category.ancestors_count * 3
+    #require 'ruby-debug'
+    #debugger
+    result += render :partial =>'admin/categories/category', :locals=> {:category => category}
+    unless children.empty? then
+      children.each { |child| result += category_tree_category(child, all_categories, config) }
+    end
+    result
+  end
+
 end
